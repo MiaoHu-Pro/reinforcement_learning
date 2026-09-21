425eba6279e3dd5307ffd5668fce283c477bdd71
Miao Hu <miao.hu@soton.ac.uk>
2026-09-21 12:14:47 +0100

    dalle2 demo

--- multip_modal/dalle2-fixed/submit-dalle2-train.sh
+++ multip_modal/dalle2-fixed/submit-dalle2-train.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --partition=a100
+#SBATCH --partition=l4
 #SBATCH --gres=gpu:1
 #SBATCH --time=60:00:00
 #SBATCH --job-name=dalle2-train
