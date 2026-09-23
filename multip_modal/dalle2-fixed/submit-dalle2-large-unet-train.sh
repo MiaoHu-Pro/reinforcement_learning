b089f61c1ab553dc0992ffdc8902b34107475202
Miao Hu <miao.hu@soton.ac.uk>
2026-09-23 14:57:45 +0100

    update evaluate

--- multip_modal/dalle2-fixed/submit-dalle2-large-unet-train.sh
+++ multip_modal/dalle2-fixed/submit-dalle2-large-unet-train.sh
@@ -7,7 +7,7 @@
 #SBATCH --cpus-per-task=8
 #SBATCH --partition=a100
 #SBATCH --gres=gpu:1
-#SBATCH --time=5-00:00:00
+#SBATCH --time=60:00:00
 #SBATCH --job-name=dalle2-large-unet
 #SBATCH --output=result_out/dalle2-large-unet-%j.out
 
